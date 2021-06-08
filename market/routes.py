7ed409db2e8a6b64f8c5c34eab1ad2eafe2ce5b7
Aju Maharjan <99ajumrzn@gmail.com>
2021-06-08 09:26:04 +0545

    completed

--- market/routes.py
+++ market/routes.py
@@ -1,4 +1,3 @@
-from werkzeug.utils import redirect
 from market import app
 from flask import request,render_template
 from pip._vendor import requests
@@ -18,31 +17,25 @@ def homepage():
 	confirmed_total= data[0]['confirmed']
 	recovered_total = data[0]['recovered'] 
 	deaths_total = data[0]['deaths']
+
 	if request.method=="POST":
+
+		 
 		country_name=request.form['code']
-	 
-		url = "https://covid-19-data.p.rapidapi.com/country" 
-		querystring = {"name":country_name}
+	  	
+		url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats"
+
+		querystring = {"country":country_name}
 
 		headers = {
-		    'x-rapidapi-key': "a1980f425bmsh0893131aab4a581p1e9ad2jsn8ec0f61d4df5",
-		    'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
-		    }
+			'x-rapidapi-key': "a1980f425bmsh0893131aab4a581p1e9ad2jsn8ec0f61d4df5",
+			'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com"
+    			}
 
 		response = requests.request("GET", url, headers=headers, params=querystring)
-		
-		print(response)
 		data=response.json()
-		print(data)
-		country=data[0]['country']
-		confirmed= data[0]['confirmed']
-		recovered = data[0]['recovered']
-		critical = data[0]['critical']
-		deaths = data[0]['deaths']
-		 
-		 
-		return render_template('data.html',country=country,confirmed=confirmed,
-		recovered=recovered,critical=critical,deaths=deaths)
+		status=data['data']['covid19Stats']
+		return render_template('homepage.html',temp=status,confirmed=confirmed_total,recovered=recovered_total,deaths=deaths_total)
 	else:
 
 		return render_template('homepage.html',confirmed=confirmed_total,recovered=recovered_total,deaths=deaths_total)
